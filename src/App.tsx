5a143ff5cce7e37bac2050be4c4fbe5a66f3b155
xAlva69 <as.gonzalez@alumno.etec.um.edu.ar>
2025-02-12 23:03:40 -0300

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Page1 from "./pages/page1.jsx"; // Importación corregida
 import Page2 from "./pages/page2.jsx";
-import Carousel from "./pages/carrusel.jsx"
+import Carousel from "./pages/carrusel.jsx";
+import Trabajos from "./pages/trabajos.jsx"
 
 function App() {
   return (
     <Router>
         <Routes>
-          <Route path="/" element={<><Page1 /><Page2/><Carousel/> </>}  />
+          <Route path="/" element={<><Page1 /><Page2/><Carousel/><Trabajos/> </>}  />
         </Routes>
     </Router>
   );
